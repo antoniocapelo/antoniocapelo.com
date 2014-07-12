d09c0e0277157bc144f3506cbaac97561b30876e
Capelo <antonio.capelo@blip.pt>
2014-07-12 18:34:57 +0100

    reiniciar

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('antoniocapeloComApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
