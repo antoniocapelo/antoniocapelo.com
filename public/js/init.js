f0db5c879b84ece589abc60420c99fa0e98f11a9
Capelo <antonio.capelo@blip.pt>
2014-08-16 12:31:54 +0100

    before removing bootstrap

--- public/js/init.js
+++ public/js/init.js
@@ -4,6 +4,11 @@ angular.element(document).ready(function() {
     //Fixing facebook bug with redirect
     if (window.location.hash === '#_=_') window.location.hash = '#!';
 
+
+    // init AWTY instance
+	var awtyInstance = new Awty();
+	awtyInstance.init();
+
     //Then init the app
     angular.bootstrap(document, ['mean']);
 });
